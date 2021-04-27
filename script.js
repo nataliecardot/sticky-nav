cc243f7db06270fd0e9b8e2d7a79dfaf9a501b55
Natalie-Cardot <nataliecardot@gmail.com>
2021-04-27 15:14:44 -0700

    Add comments

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let topOfNav = nav.offsetTop;
 
 function fixNav() {
   if (window.scrollY >= topOfNav) {
+    // Prevent div below nav jumping up into extra space after nav becomes position fixed and is removed from document flow (and thus space is no longer created for it in page layout)
     document.body.style.paddingTop = nav.offsetHeight + 'px';
     document.body.classList.add('fixed-nav');
   } else {
